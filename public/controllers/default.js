0264b3760886fde7d1b0f8591f569e57e0fe0967
apricots <potatonisha@gmail.com>
2015-02-01 16:51:30 -0800

    stuff

--- public/controllers/default.js
+++ public/controllers/default.js
@@ -1,22 +1,101 @@
 app.controller('DefaultCtrl', function ($scope, $location) {
 
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here https://dashboard.stripe.com/account
-var stripe = require("stripe")("sk_test_DPr34ac2HzBS4QKWr3aQen3C");
 
-// (Assuming you're using express - expressjs.com)
-// Get the credit card details submitted by the form
-var stripeToken = request.body.stripeToken;
 
-var charge = stripe.charges.create({
-  amount: 1000, // amount in cents, again
-  currency: "usd",
-  card: stripeToken,
-  description: "payinguser@example.com"
-}, function(err, charge) {
-  if (err && err.type === 'StripeCardError') {
-    // The card has been declined
-  }
-});
+  $scope.collections = [
+    {
+      "title1": "NYC",
+      "title2": "Subway",
+      "description": "Dave is glad the 7 is running this weekend.",
+      "cover": "images/subway1.png"
+    },
+    {
+      "title1": "Beach",
+      "title2": "Bums",
+      "description": "Dave sure could use a tan.",
+      "cover": "images/beach.png"
+    },
+    {
+      "title1": "Yelp",
+      "title2": "Adventures",
+      "description": "Dave rejoices in the fact that he lives in the culinary nexus of the world.",
+      "cover": "images/sushi.png"
+    },
+    {
+      "title1": "Nice",
+      "title2": "Shot",
+      "description": "Dave has the patience of a jaguar and the aim of a falcon.",
+      "cover": "images/gun.png"
+    },
+    {
+      "title1": "Hot",
+      "title2": "Pot",
+      "description": "Dave prefers the lamb to the beef.",
+      "cover": "images/soup.png"
+    },
+    {
+      "title1": "Construction",
+      "title2": "Work",
+      "description": "If Dave wants something to get done, he must do it himself.",
+      "cover": "images/hat.png"
+    },
+    {
+      "title1": "Charades",
+      "title2": "Game",
+      "description": "Dave prefers poker because it is more intellectually stimulating.",
+      "cover": "images/game.png"
+    },
+    {
+      "title1": "Corporate",
+      "title2": "Life",
+      "description": "Dave hates Mondays.",
+      "cover": "images/sad.png"
+    },
+    {
+      "title1": "Going",
+      "title2": "Home",
+      "description": "Dave can't wait to get back to his foam queen-sized mattress and five pillows.",
+      "cover": "images/train.png"
+    },
+    {
+      "title1": "Dance",
+      "title2": "Party",
+      "description": "Dave pretends to enjoy beer and whiskey in order to fit in.",
+      "cover": "images/show.png"
+    },
+
+    {
+      "title1": "San",
+      "title2": "Francisco",
+      "description": "Dave loves the great outdoors, so long as he remembers to wear sunblock.",
+      "cover": "images/sf.png"
+    },
+    {
+      "title1": "Dinner",
+      "title2": "Date",
+      "description": "Dave resents getting his picture taken.",
+      "cover": "images/dinner.png"
+    },
+
+    {
+      "title1": "Kitchen",
+      "title2": "Fun",
+      "description": "Dave has been meaning to try out this recipe.",
+      "cover": "images/bake.png"
+    },
+    {
+      "title1": "Cultural",
+      "title2": "Festivities",
+      "description": "Dave finds Thai culture rather fascinating.",
+      "cover": "images/monks.png"
+    },
+    {
+      "title1": "Social",
+      "title2": "Media",
+      "description": "Dave can't wait to see how many likes he can gather.",
+      "cover": "images/sub.png"
+    },
+  ];
+
 
 });
